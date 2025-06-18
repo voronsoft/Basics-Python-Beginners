65fd5cad1400e57a3f3d403d40d3967788f4450a
voronsoft <gbo4.net@gmail.com>
2025-06-19 00:44:37 +0300

    Рефакторинг тестов главы 9_4

--- tests/test_9_4_5.py
+++ tests/test_9_4_5.py
@@ -5,6 +5,9 @@ import sys
 
 from io import StringIO
 
+from utils.code_security_check import check_code_safety
+from utils.stdin_stdout_stderr_interceptor import stream_interceptor
+
 
 def test_9_4_5(path_tmp_file: str, task_num_test: str):
     """Тестирование структуры кода"""
@@ -17,6 +20,8 @@ def test_9_4_5(path_tmp_file: str, task_num_test: str):
         # Чтение пользовательского кода
         with open(path_tmp_file, "r", encoding="utf-8") as f:
             code = f.read()
+        # Безопасность кода пользователя: читаем код и проверяем его до запуска
+        check_code_safety(code)
 
         # Разбор кода в дерево AST
         tree = ast.parse(code)
@@ -75,25 +80,12 @@ def test_9_4_5_1(path_tmp_file: str):
             spec = importlib.util.spec_from_file_location("user_module", path_tmp_file)
             user_module = importlib.util.module_from_spec(spec)
 
-            original_stdin = sys.stdin
-            original_stdout = sys.stdout
+            # Используем контекстный менеджер для подмены потоков
+            with stream_interceptor(stdin_data=test_input[i], capture_stdout=True, capture_stderr=True) as streams:
+                spec.loader.exec_module(user_module)  # Выполняем код модуля
 
-            # Подменяем stdin с тестовыми данными
-            sys.stdin = StringIO(test_input[i])
-            # Заглушка для sys.stderr
-            original_stderr = sys.stderr  # сохраняем оригинал
-            sys.stderr = StringIO()  # подменяем на буфер
-            # Создаем буфер для перехвата вывода
-            output_buffer = StringIO()
-
-            # Перенаправляем stdout в буфер
-            sys.stdout = output_buffer
-
-            # Выполняем пользовательский модуль
-            spec.loader.exec_module(user_module)
-
-            # Получаем перехваченный вывод из print()
-            captured_output = output_buffer.getvalue().strip()
+            # Получаем перехваченный вывод из stdout
+            captured_output = streams["stdout"].getvalue().rstrip() if streams["stdout"] else ""
 
             # Формируем отчет по тесту
             test_result = []
